bd9a8aa5f13166a322aca4034b86862099bbbf1c
Viacheslav Panchuk <pevmel@gmail.com>
2019-03-13 11:19:22 +0200

    Added broadcast route

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "categories#index"
 
+  mount ActionCable.server, at: "/cable"
 end
